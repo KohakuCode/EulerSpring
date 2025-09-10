38b835ef8de8ef8457cf663adc808e9445e92e92
Letizia <tizicravotta@gmail.com>
2025-09-10 13:16:25 +0200

    added Euler8

--- src/main/java/com/Kohaku/EulerSpring/HomeController.java
+++ src/main/java/com/Kohaku/EulerSpring/HomeController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class HomeController {
 
     @Autowired
+    private Euler8 euler8;
+    @Autowired
     private Euler7 euler7;
     @Autowired
     private Euler6 euler6;
@@ -27,6 +29,11 @@ public class HomeController {
     public ResponseEntity<Long> test(@PathVariable(value = "euler") int euler){
         System.out.println(euler7.getPrimeNumber(euler));
         return ResponseEntity.ok(euler7.getPrimeNumber(euler));
+    }
+    @GetMapping(value="/euler8/", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Long> euler8(){
+        System.out.println(euler8.getLargestProduct());
+        return ResponseEntity.ok(euler8.getLargestProduct());
     }
 
 
